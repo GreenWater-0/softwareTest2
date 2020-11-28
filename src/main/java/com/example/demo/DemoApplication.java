dbbb045fb592aca6ee4313025d4f966e6a23dd19
syw <2078901355@qq.com>
2020-11-29 03:03:13 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) throws Exception{
+		try {
+			SpringApplication.run(DemoApplication.class, args);
+		} catch (Exception e){
+			System.out.println("错误"+e);
+		}
+	}
+
+}
